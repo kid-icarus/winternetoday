a2af2c52a5000a649578f23e7c4bb7d5ecd217cf
Ryan Kois <ryan.kois@gmail.com>
2013-11-19 19:45:00 -0700

    Add jquery.

--- api.php
+++ api.php
@@ -5,7 +5,7 @@ $data = array(
   'twitter_ids' => array('402987048086142976', '402982981067104256'),
 );
 
-return json_encode($data);
+print json_encode($data);
 
 // <blockquote class="twitter-tweet" lang="en"><p>I&#39;m gonna go have sex now bye twitter</p>&mdash; Elektra Luxx (@ElektraLuxxx) <a href="https://twitter.com/ElektraLuxxx/statuses/402987048086142976">November 20, 2013</a></blockquote>
 //   <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
